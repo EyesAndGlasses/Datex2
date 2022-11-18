beba040b312467b769811d4e4b07887304df956f
Catherine2000 <katerinapanina@icloud.com>
2022-11-18 11:44:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
